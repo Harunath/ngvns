7204a8d9e8c2120d614a41558a59b8d397df14b1
sushmasri1906 <sushmasri1906@gmail.com>
2025-08-05 11:23:42 +0530

    Pages updated

---
+++ apps/ngvns-user-app/src/app/(main)/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactMain from "../../../components/contact/ContactMain";
+import FAQSection from "../../../components/contact/Faq";
+
+function page() {
+	return (
+		<>
+			<ContactMain />
+			<FAQSection />
+		</>
+	);
+}
+
+export default page;
